1f79b30782df56ae698c23e7cfc0371836e25a54
Kent Carmine <kentcarmine@gmail.com>
2018-01-23 22:23:32 -0600

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ let socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: "ClientExample",
-    text: "This message was sent from the client"
-  });
+  // socket.emit('createMessage', {
+  //   from: "ClientExample",
+  //   text: "This message was sent from the client"
+  // });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -17,14 +17,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'ServerExample',
-    text: 'This is an example message from the server',
-    createdAt: Date.now()
-  });
-
   socket.on('createMessage', (message) => {
     console.log("createMessage: ", message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   })
 
   socket.on('disconnect', () => {
